51cdf4ccbed783e221638f0c011f2f08a5775ca4
jondavid327 <jondavid327@gmail.com>
2019-06-03 16:48:13 -0400

    Add files via upload

---
+++ papagayo.shop/index.php
@@ -0,0 +1,19 @@
+<!-- header include menu social networks and search -->
+<?php include 'header/header.php';?>
+<!-- header include menu social networks and search -->
+
+
+		<!-- gallerys -->
+			
+		<?php include 'gallery/main_gallery.php';?>
+		<?php include 'gallery/new_products.php';?>
+		<?php include 'gallery/deals.php';?>
+		<?php include 'gallery/top_selling.php';?>
+		<?php include 'gallery/tree_gallery.php';?>
+
+		<!-- gallerys -->
+		
+	
+		<!-- SECTION -->
+		
+<?php include 'footer/footer.php';?>
